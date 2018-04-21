81a2d7f9f5779b4e80251b8dd358654c0980acd8
permanar <richie.permana@gmail.com>
2018-04-21 18:24:12 +0800

    Changing the static menu to be dinamic.

--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -101,7 +101,30 @@
             </div>
           </div>
         </div>
-        @yield('topbar')
+        <div class="header collapse d-lg-flex p-0" id="headerMenuCollapse">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-lg-3 ml-auto">
+                  <form class="input-icon my-3 my-lg-0">
+                    <input type="search" class="form-control header-search" placeholder="Search&hellip;" tabindex="1">
+                    <div class="input-icon-addon">
+                      <i class="fe fe-search"></i>
+                    </div>
+                  </form>
+                </div>
+                <div class="col-lg order-lg-first">
+                  <ul class="nav nav-tabs border-0 flex-column flex-lg-row">
+                    <li class="nav-item">
+                      <a href="{!! url('/home') !!}" class="nav-link"><i class="fe fe-home"></i> Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                      <a href="{!! url('/profile') !!}" class="nav-link"><i class="fe fe-user"></i> Profile</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+        </div>
         <div class="my-3 my-md-5">
           <div class="container">
             @yield('content')
